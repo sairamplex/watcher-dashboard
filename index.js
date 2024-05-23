f75554b4b87762006ea610cef5650117a42cef77
Sairam kola <sai@plex.engineer>
2024-05-23 18:46:32 +0530

    refactor

--- index.js
+++ index.js
@@ -5,82 +5,47 @@ function formatField(field) {
         .join(' ');
 }
 
-const fetchHyperLiquidData = async () => {
+const fetchData = async (source) => {
     try {
-        let response = await fetch(`https://autovault-g9v7.onrender.com/hl-get-data`, {
+        let response = await fetch(`https://autovault-g9v7.onrender.com/${source}-get-data`, {
             headers: {
                 'Content-type': 'application/json'
             }
         })
         if (response.status !== 200) {
-            console.log("Cannot get hyperliquid data!", error)
-            throw "Cannot get hyperliquid data"
+            console.log(`Cannot get ${source} data!`, error)
+            throw `Cannot get ${source} data`
         }
         const result = await response.json();
-        document.getElementById('hl-message').textContent = ''
-        document.querySelector('#hl-loader').hidden = true;
-        const tbody = document.querySelector('#hyperliquid');
+        document.getElementById(`${source}-message`).textContent = ''
+        document.getElementById(`${source}-loader`).hidden = true;
+        const tbody = document.getElementById(`${source}`);
         tbody.innerHTML = '';
         for(const field in result){
             const tr = document.createElement('tr');
             const td1 = document.createElement('td');
-            td1.textContent = formatField(field)
+            td1.textContent = formatField(field);
             tr.appendChild(td1);
             const td2 = document.createElement('td');
-            td2.textContent = Number(result[field]).toFixed(5)
+            td2.textContent = Number(result[field]).toFixed(5);
             tr.appendChild(td2);
             tbody.appendChild(tr);
         }
 
     } catch (error) {
-        document.querySelector('#hyperliquid').innerHTML = '';
-        document.querySelector('#hl-loader').hidden = true;
-        document.getElementById('hl-message').textContent = 'Cannot get data'
-        console.log("Cannot get hyperliquid data",error)
-    }
-  };
-
-  const fetchAaveLiquidData = async () => {
-    try {
-        let response = await fetch(`https://autovault-g9v7.onrender.com/aave-get-data`, {
-            headers: {
-                'Content-type': 'application/json'
-            }
-        })
-        if (response.status !== 200) {
-            console.log("Cannot get aave data!",error)
-            throw "Cannot get aave data"
-        }
-        const result = await response.json();
-        document.getElementById('aave-message').textContent = ''
-        document.querySelector('#aave-loader').hidden = true;
-        const tbody = document.querySelector('#aave');
-        tbody.innerHTML = '';
-    
-        for(const field in result){
-            const tr = document.createElement('tr');
-            const td1 = document.createElement('td');
-            td1.textContent = formatField(field)
-            tr.appendChild(td1);
-            const td2 = document.createElement('td');
-            td2.textContent = Number(result[field]).toFixed(5)
-            tr.appendChild(td2);
-            tbody.appendChild(tr);
-        }
-    } catch (error) {
-        document.querySelector('#aave').innerHTML = '';
-        document.querySelector('#aave-loader').hidden = true;
-        document.getElementById('aave-message').textContent = 'Cannot get data'
-        console.log("Cannot get aave data",error)
+        document.getElementById(`${source}`).innerHTML = '';
+        document.getElementById(`${source}-loader`).hidden = true;
+        document.getElementById(`${source}-message`).textContent = 'Cannot get data'
+        console.log(`Cannot get ${source} data`,error)
     }
   };
 
   window.addEventListener('load', () => {
-    document.querySelector('#hl-loader').hidden = false;
-    document.querySelector('#aave-loader').hidden = false;
-    fetchHyperLiquidData();
-    fetchAaveLiquidData();
-    setInterval(fetchHyperLiquidData, 5000);
-    setInterval(fetchAaveLiquidData, 5000);
+    document.getElementById('hl-loader').hidden = false;
+    document.getElementById('aave-loader').hidden = false;
+    fetchData("hl");
+    fetchData("aave");
+    setInterval(() => fetchData("hl"), 5000);
+    setInterval(() => fetchData("aave"), 5000);
 });
   
